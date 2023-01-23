55584dccdae7d284ea84cd52085a32801b7fe113
Zuzanna Komendecka <z.komendecka@gmail.com>
2023-01-23 11:08:16 +0100

    10.1 DatePicker HourPicker class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,12 +1,15 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
-  constructor (element) {
+  constructor(element) {
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
   }
+
   render(element) {
     const thisBooking = this;
 
@@ -16,19 +19,25 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
+
   initWidgets() {
     const thisBooking = this;
-    
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
-    thisBooking.dom.peopleAmount.addEventListener('updated', function () {});
-
+    thisBooking.dom.peopleAmount.addEventListener('updated', function () {
+    });
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
-    thisBooking.dom.peopleAmount.addEventListener('updated', function (){});
-
-
-
+    thisBooking.dom.peopleAmount.addEventListener('updated', function () {
+    });
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.dom.datePicker.addEventListener('updated', function () {
+    });
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.dom.hourPicker.addEventListener('updated', function () {
+    });
   }
 }
+
 export default Booking;
